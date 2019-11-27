511584f4ccdedecc56f569371dc8cf469bdd5efe
zhongjian <gzzhongjian@yonyou.com>
2019-11-27 17:31:59 +0800

    test

---
+++ build/precache-manifest.a839145ab7dedab9d0107790238cd0af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553f12b7ccf6969445ff2718ebe265fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920aa8422b36e7e7f4c4",
+    "url": "/static/css/2.90b83120.chunk.css"
+  },
+  {
+    "revision": "c16981d3cd580413e321",
+    "url": "/static/css/main.99dfb968.chunk.css"
+  },
+  {
+    "revision": "920aa8422b36e7e7f4c4",
+    "url": "/static/js/2.bf711fb0.chunk.js"
+  },
+  {
+    "revision": "c16981d3cd580413e321",
+    "url": "/static/js/main.fc06f02a.chunk.js"
+  },
+  {
+    "revision": "8695f89d1c3f6208f9b7",
+    "url": "/static/js/runtime-main.569759db.js"
+  }
+]);
